0b2fbf2752fdd6a1f6dca18de157a087136c5ffd
Vincent WENDLING <vincent@starling-burgers.com>
2022-08-02 18:01:39 +0200

    feat: started working on the stats page

--- src/pages/Statistiques.tsx
+++ src/pages/Statistiques.tsx
@@ -1,8 +1,25 @@
 const Statistiques = () => {
   return (
-    <>
-      <h1>Statistiques</h1>
-    </>
+    <div className="w-3/4 flex flex-col gap-4 m-auto">
+      <h1 className="text-2xl font-bold mt-8 w-full text-center">
+        Statistiques
+      </h1>
+      <form action="" className="flex justify-center gap-2">
+        <label htmlFor="" className="input-group w-fit">
+          <span>Produit</span>
+          <select className="select select-bordered"></select>
+        </label>
+        <label htmlFor="" className="input-group w-fit">
+          <span>Timeframe</span>
+          <select className="select select-bordered">
+            <option value="day">Jour</option>
+            <option value="week">Semaine</option>
+            <option value="Month">Mois</option>
+          </select>
+        </label>
+        <button className="btn btn-primary btn-outline">Afficher</button>
+      </form>
+    </div>
   );
 };
 
